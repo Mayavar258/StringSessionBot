ffc5f175352991267df0a07b126e1517ac4282d5
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-18 08:37:38 +0000

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "23134975").strip()
+API_HASH = os.getenv("API_HASH", "5c0a4b4bafb0418032faf4ec845a4df4").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6548114637:AAH9BQaGfv-Ml9-0oFItXP8Vj7HrxCmxbWI").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://ragav:ragav@cluster0.mv2hczu.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/good_nation")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
