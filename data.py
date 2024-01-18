82bb23958d68571caa819bf8dca2a1f080bb1b89
Mayavar258 <123068420+Mayavar258@users.noreply.github.com>
2024-01-18 08:02:28 +0000

    Update data.py

--- data.py
+++ data.py
@@ -13,12 +13,12 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("✨SUPPORT✨", url="https://t.me/technomindzchat")],
+        [InlineKeyboardButton("✨SUPPORT✨", url="https://t.me/good_nation")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("About🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/technomindzbot")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/good_nation")],
     ]
 
     START = """
@@ -33,7 +33,7 @@ If you don't trust this bot,
 Still reading?
 You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
 
-By @TmMainChannel
+By @good_nation
     """
 
     HELP = """
@@ -50,13 +50,13 @@ By @TmMainChannel
     ABOUT = """
 **About This Bot** 
 
-Telegram Bot to generate Pyrogram and Telethon string session by @StarkBots
+Telegram Bot to generate Pyrogram and Telethon string session by @good_nation
 
-Source Code : [Click Here](https://t.me/technomindzbot)
+Source Code : [Click Here](https://t.me/good_nation)
 
 Framework : [Pyrogram](https://docs.pyrogram.org)
 
 Language : [Python](https://www.python.org)
 
-Developer : [꧁𓊈𒆜🆁🅰🆅🅸🅽🅰🅽🅳🅰🅽𒆜𓊉꧂](https://t.me/technomindzyt)
+Developer : [꧁𓊈𒆜🆁🅰🆅🅸🅽🅰🅽🅳🅰🅽𒆜𓊉꧂](https://t.me/good_nation)
     """
